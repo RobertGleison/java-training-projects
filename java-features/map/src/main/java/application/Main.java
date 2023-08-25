de5f1f2c22b908d5786275eaae93f5529bc8b03c
Robert Gleison <robertgleison12@gmail.com>
2023-08-25 10:48:52 +0100

    Finished the code

--- java-features/map/src/main/java/application/Main.java
+++ java-features/map/src/main/java/application/Main.java
@@ -1,7 +1,6 @@
 package application;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Map;
@@ -10,25 +9,38 @@ import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Map<String,Integer> votes = new TreeMap<>();
 
-        System.out.println("Enter full file path: ");
+        Scanner sc = new Scanner(System.in);
+        Map<String, Integer> votes = new TreeMap<>();
+        System.out.print("Enter full file path: ");
         String path = sc.nextLine();
+
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line = br.readLine();
-            while (line != null ){
+            while (line != null) {
                 String[] fields = line.split(",");
-                votes.put(fields[0],Integer.valueOf(fields[1]));
+                if (fields.length >= 2) {
+                    String candidate = fields[0];
+                    int candidateVotes = Integer.parseInt(fields[1]);
+                    if (!votes.containsKey(fields[0])) {
+                        votes.put(fields[0], Integer.valueOf(fields[1]));
+                    } else {
+                        int currentVotes = votes.get(candidate);
+                        int newVotes = currentVotes + candidateVotes;
+                        votes.put(candidate, newVotes);
+                    }
+                }
                 line = br.readLine();
             }
 
-
+            System.out.println();
+            for (String key : votes.keySet()) {
+                System.out.println(key + ": " + votes.get(key));
+            }
         }
-        catch (IOException e){
+        catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
-
         sc.close();
     }
 }
